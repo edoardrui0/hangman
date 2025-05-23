685009c494eaaed5c3fe5e3f1b6f6d91e006e97e
Edoardo Ruiz <44981900+edoardrui0@users.noreply.github.com>
2025-05-23 18:29:59 -0400

    added serialization features
    able to load saved games and convert data to JSON

--- serialize.rb
+++ serialize.rb
@@ -1,2 +1,36 @@
 module Serialize
+  # Checks for saved_game file then displays all saved games to user
+  def load_game
+    return unless File.exist?('saved_games')
+
+    puts "Here's a list of the saved games: #{Dir.children('saved_games')}"
+    Dir.children('saved_games').each { |file| puts "- #{file}" }
+    puts 'Please input a filename you wish to load i.e "filename.json"'
+    filename = gets.chomp
+    from_json(filename)
+  end
+
+  # Loads selected save file and parses it from JSON
+  def from_json(filename)
+    f = JSON.parse(File.read("saved_games/#{filename}")) # might need to symbolize_names: true
+    @word = f[:word]
+    @display = f[:display]
+    @life = f[:life]
+    @player.misses = f[:player_misses]
+    @player.guess_history = f[:player_guess_history]
+  end
+
+  # Converts current game into JSON
+  def to_json(filename)
+    Dir.mkdir('saved_games') unless File.exist?('saved_games')
+    File.open("saved_games/#{filename}", 'w') do |f|
+      JSON.dump({
+                  word: @word,
+                  display: @display,
+                  life: @life,
+                  player_misses: @player.misses,
+                  player_guess_history: @player.guess_history
+                }, f)
+    end
+  end
 end
