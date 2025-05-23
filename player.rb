6c453cfb66d260b3ebdf3cec1083a77da70c4eca
Edoardo Ruiz <44981900+edoardrui0@users.noreply.github.com>
2025-05-23 15:18:22 -0400

    Completed Player.rb
    Gets input from player, then validates if its a letter
    If validated, checks if letter has been previous selected

--- player.rb
+++ player.rb
@@ -1,2 +1,43 @@
 class Player
+  attr_accessor :guess, :guess_history, :misses
+
+  def initialize
+    @guess = ''
+    @guess_history = []
+    @misses = []
+  end
+
+  # get input, then check whether to save game or validate guess
+  def player_input
+    input = gets.chomp
+    if input == 'save'
+      @guess = input
+    else
+      validate_input(input)
+    end
+  end
+
+  # check if user's input is within range
+  def validate_input(input = '')
+    until input.length == 1 && input =~ /[a-z]/
+      puts 'Please input a letter between a-z'
+      input = gets.chomp.downcase
+    end
+    @guess = input
+    check_history(input)
+    @guess_history << @guess
+  end
+
+  # checks if input has previously been selected
+  def check_history(input)
+    if @guess_history.include?(input)
+      puts 'You have already selected that letter'
+      validate_input
+    else
+      false
+    end
+  end
 end
+
+# new = Player.new
+# new.player_input
